cbac3c819e788f92f448adcc2652dcae5e953dcc
johntran <tranjohn1211@gmail.com>
2022-03-28 21:27:47 -0700

    first commit for movie api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect} from 'react'
+import axios from 'axios';
 
 function App() {
+  const [movieInfo, setMovieInfo] = useState({})
+  const [search, setSearch] = useState('shrek')
+  const [input, setInput] = useState('')
+
+  useEffect(() => {
+    // OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=61a95ef8
+ 
+
+    async function fetchMovieData () {
+      const movieUrl = `http://www.omdbapi.com/?t=${search}&apikey=61a95ef8`
+      const response = await axios.get(movieUrl)
+      setMovieInfo(response.data)
+    
+    }
+    
+    fetchMovieData()
+
+  }, [search])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setSearch(input)
+    // console.log('hi')
+    setInput('')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+      <img src={movieInfo.Poster} alt={movieInfo.Title}/>
+      <h3>Title: {movieInfo.Title}</h3>
+      <p>Release date: {movieInfo.Released}</p>
+    </main>
+
+    <form onSubmit={handleSubmit} >
+      <label htmlFor='movieIfno'>Title: </label>
+      <input id='movieInfo'  value= {input} onChange={(e)=>{setInput(e.target.value)}}/>
+    </form>
     </div>
   );
 }
